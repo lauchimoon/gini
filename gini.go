a6b7cae22cfb40ee0ce391b41c8ba3f71af32862
lauchimoon <laurentino.luna06@gmail.com>
2026-02-21 14:02:45 -0300

    Add Dump function

--- gini.go
+++ gini.go
@@ -3,6 +3,7 @@ package gini
 import (
     "fmt"
     "io"
+    "sort"
     "os"
 )
 
@@ -33,6 +34,31 @@ func NewFromString(iniString string) (*Ini, error) {
     l := &lexer{Source: iniString, LenSource: len(iniString), Cursor: 0}
     p := &parser{Tokens: l.lex(), Cursor: 0}
     return p.parse()
+}
+
+func Dump(data map[string]map[string]string, outFile io.Writer) {
+    sections := []string{}
+    for section := range data {
+        sections = append(sections, section)
+    }
+    sort.Strings(sections)
+
+    for i, section := range sections {
+        fmt.Fprintf(outFile, "[%s]\n", section)
+        keys := []string{}
+        for key := range data[section] {
+            keys = append(keys, key)
+        }
+
+        for _, key := range keys {
+            fmt.Fprintln(outFile, key, "=", data[section][key])
+        }
+
+        // Don't write last newline
+        if i < len(sections) - 1 {
+            fmt.Fprintln(outFile)
+        }
+    }
 }
 
 func (ini *Ini) Get(section, key string) (string, error) {
